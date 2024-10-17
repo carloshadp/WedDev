88523ffb07b80e3d21e0a97da7a2e0541a2c0a6b
Carlos henrique <carloshadp@gmail.com>
2024-10-16 22:44:55 -0300

    aprendendo fetch

--- 2 Semestre/Aula 5/Portal-filmes/src/components/MovieCard.jsx
+++ 2 Semestre/Aula 5/Portal-filmes/src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 
-export default function MovieCard({id, titulo, imagem_destaque}) {
+export default function MovieCard({id, title, backdrop_path,poster_path,  vote_average}) {
     return(
         <>
         <div>
-            <h2>{titulo}</h2>
-            <img src={imagem_destaque} />
+            <h2>{title}</h2>
+            <p>⭐{vote_average}</p>
+            <img src={`https://image.tmdb.org/t/p/w154${poster_path}`} alt={title} />
             <Link to={`/movies/${id}`}>Saiba mais</Link>
         </div>
         
--- 2 Semestre/Aula 5/Portal-filmes/src/pages/MovieListPage.jsx
+++ 2 Semestre/Aula 5/Portal-filmes/src/pages/MovieListPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 import movies from "../data/movies.json"
 
@@ -6,13 +6,26 @@ import movies from "../data/movies.json"
 export default function MovieListPage(){
 
     const [search, setSearch] = useState("")
+    const [filmes, setFilmes] = useState([])
 
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br")
+        .then( data => data.json())
+        .then( data_json => setFilmes(data_json.results))
+        .catch( erro => console.log(erro) )
+        .finally(() => console.log("Cabo =("))
+
+    }, [])
+
+
+    
+    
     const handleSearch = (event) => {
         setSearch(event.target.value)
         console.log(search)
     }
 
-    const filmesFiltrados = movies.filter( filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+    const filmesFiltrados = filmes.filter( filme => filme.title.toLowerCase().includes(search.toLowerCase()))
     return(
         <>
         <h2>Veja o catálogo completo de filmes</h2>
@@ -23,7 +36,7 @@ export default function MovieListPage(){
                 value={search}
                 onChange={handleSearch}
             />
-            <section className="flex">
+            <section className="">
                 {
                     
 
